c387b482dec1b82ae9f0bd81b99b68c71c4e0efa
Gaurav Upadhyay <gauravupadhyay.work@gmail.com>
2026-07-30 15:11:04 +0530

    feat: initial commit

---
+++ src/test/java/com/rupeex/main/RupeeXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupeex.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RupeeXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
